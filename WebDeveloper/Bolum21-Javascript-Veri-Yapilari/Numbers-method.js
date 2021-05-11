e0a12b865d78b6212e070273182b649f03f13f5e
Diyar Yıldırım <Diyary2000@gmail.com>
2021-05-11 20:50:05 +0300

    New Lesson

---
+++ WebDeveloper/Bolum21-Javascript-Veri-Yapilari/Numbers-method.js
@@ -0,0 +1,7 @@
+// Numbers
+
+let val;
+val = Number('10');
+
+console.log(val);
+console.log(typeof val);
--- WebDeveloper/Bolum21-Javascript-Veri-Yapilari/Uygulama-Date.js
+++ WebDeveloper/Bolum21-Javascript-Veri-Yapilari/Uygulama-Date.js
@@ -21,5 +21,41 @@ console.log(dtC);
 // iki tarih arasındaki geçen zamanı bulunuz.
 
 const start = new Date('1/1/1990');
-const end = new Date('1/1/1991');
+const end = new Date('2/10/1991');
 
+var milisecond = end - start;
+var saniye = milisecond/1000;
+var dakika = saniye / 60;
+var saat= dakika / 60;
+var gun = saat/24;
+
+console.log('milisecond :' + milisecond);
+console.log('saniye : ' + saniye);
+console.log('dakika : ' + dakika);
+console.log('saat : ' + saat);
+console.log('gun : ' + gun);
+
+// Her yıl mayıs ayının 2.haftası olan pazar günü kutlanan anneler günü 2019 yılında ne zaman kutlanacaktır ?
+ var annelerGUNU = new Date();
+ annelerGUNU.setHours(0,0,0,0);
+ annelerGUNU.setFullYear(2019);
+ annelerGUNU.setDate(1);
+ annelerGUNU.setMonth(4);
+
+ while(annelerGUNU.getDay() != 0){
+     annelerGUNU.setDate(annelerGUNU.getDate()+ 1)
+ }
+annelerGUNU.setDate(annelerGUNU.getDate()+7);
+ console.log(annelerGUNU);
+
+
+// ** Yaş hesaplama nasıl yapılır ?
+
+var birthday = new Date ('8/1/1985');
+var ageDifMs= Date.now() - birthday.getTime();
+var ageDate= new Date(ageDifMs);
+
+console.log(ageDate.getFullYear()-1970);
+
+//console.log(birthday.getTime());
+//console.log(Date.now());
